0781acb9ffb3261bad1d3ad06ba9eef791d3d61a
Adan Palacios Avelar <avelaradan2810@gmail.com>
2021-09-30 19:35:54 -0500

    OneShotLinearRegression

---
+++ HandsOn3/OSLinearRegression/src/oslinearregression/OneShotAgent.java
@@ -0,0 +1,34 @@
+package oslinearregression;
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import javax.swing.JOptionPane;
+
+public class OneShotAgent extends Agent {
+
+    @Override
+    protected void setup() {
+        System.out.println("Agent "+getLocalName()+" started.");
+        addBehaviour(new MyOneShotBehaviour());
+    }
+
+    private class MyOneShotBehaviour extends OneShotBehaviour {
+
+        @Override
+        public void action() {
+            LinearRegression object = new LinearRegression();
+            object.setAdvertising(Integer.valueOf(JOptionPane.showInputDialog("Insert value for X: ")));
+            object.LinearRegression();
+            System.out.println("y = B0 + B1X1");
+            System.out.println("y = " + String.valueOf(object.getB0()) + 
+                " + (" + String.valueOf(object.getB1()) +
+                ")("+String.valueOf(object.getAdvertising()) + ")");
+            System.out.println("y = " + String.valueOf(object.getSales()));
+        }
+
+        @Override
+        public int onEnd() {
+            myAgent.doDelete();
+            return super.onEnd();
+        }
+    }
+}
