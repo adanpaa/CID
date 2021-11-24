bab33305bf95f4acd6bb6d9070b803aedf57a6f0
Adan Palacios Avelar <avelaradan2810@gmail.com>
2021-11-24 11:14:41 -0600

    Hands On 7 problema solucionado

--- HandsOn7/LogisticRegression/src/oslinearregression/LogisticRegression.java
+++ HandsOn7/LogisticRegression/src/oslinearregression/LogisticRegression.java
@@ -6,7 +6,7 @@ public class LogisticRegression {
     private final List<Double> x1 = Arrays.asList(1.0,1.0,1.0);
     private final List<Double> x2 = Arrays.asList(1.0,4.0,2.0);
     private final List<Double> x3 = Arrays.asList(1.0,2.0,4.0);
-    private List<Double> w = Arrays.asList(0.0,0.0,0.0);
+    private final List<Double> w = Arrays.asList(0.0,0.0,0.0);
     private final Double y1 = 0.0;
     private final Double y2 = 1.0;
     private final Double y3 = 1.0;
@@ -20,9 +20,12 @@ public class LogisticRegression {
         
     public void gradientDescent() {
         for(int i = 0; i < iterations; i++){
-            for(int j = 0; j < 3; j++){
-                w.set(j, w.get(j) - (learningR*(((calculateS(x1)-y1)*x1.get(j)) + ((calculateS(x2)-y2)*x2.get(j)) + ((calculateS(x3)-y3)*x3.get(j)))));
-            }
+            Double sigmoidx1 = calculateS(x1);
+            Double sigmoidx2 = calculateS(x2);
+            Double sigmoidx3 = calculateS(x3);
+            w.set(0, w.get(0) - (learningR*(((sigmoidx1-y1)*x1.get(0)) + ((sigmoidx2-y2)*x2.get(0)) + ((sigmoidx3-y3)*x3.get(0)))));
+            w.set(1, w.get(1) - (learningR*(((sigmoidx1-y1)*x1.get(1)) + ((sigmoidx2-y2)*x2.get(1)) + ((sigmoidx3-y3)*x3.get(1)))));
+            w.set(2, w.get(2) - (learningR*(((sigmoidx1-y1)*x1.get(2)) + ((sigmoidx2-y2)*x2.get(2)) + ((sigmoidx3-y3)*x3.get(2)))));
         }
     }
     
